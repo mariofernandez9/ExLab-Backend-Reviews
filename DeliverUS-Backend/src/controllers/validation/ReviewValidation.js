032293a3685510b3d2aa4049f54e0b56692d9bae
mariofernandez9 <mariofdezus@gmail.com>
2026-03-24 17:50:07 +0100

    Ejercicio3

--- DeliverUS-Backend/src/controllers/validation/ReviewValidation.js
+++ DeliverUS-Backend/src/controllers/validation/ReviewValidation.js
@@ -1,9 +1,12 @@
 import { check } from 'express-validator'
 
 const create = [
+    check('stars').exists().isInt({ min: 0, max: 5 }),
+    check('body').optional({nullable: true, checkFalsy: true}).isString().isLength({min: 1, max: 255 }).trim()
 ]
-
 const update = [
+    check('stars').exists().isInt({ min: 0, max: 5 }),
+    check('body').optional({nullable: true, checkFalsy: true}).isString().isLength({min: 1, max: 255 }).trim()
 ]
 
 export { create, update }
